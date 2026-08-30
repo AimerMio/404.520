3cb25c4b4d339bd8e98a457ecae0636aee9d6e02
AimerMio <ABC20111222@OUTLOOK.COM>
2026-08-31 01:01:56 +0800

    Update astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -1,5 +1,5 @@
 import { rehypeHeadingIds } from '@astrojs/markdown-remark'
-import vercel from '@astrojs/vercel'
+// import vercel from '@astrojs/vercel'
 import AstroPureIntegration from 'astro-pure'
 import { defineConfig, fontProviders, svgoOptimizer } from 'astro/config'
 import rehypeKatex from 'rehype-katex'
@@ -40,11 +40,11 @@ export default defineConfig({
 
   // [Adapter]
   // https://docs.astro.build/en/guides/deploy/
-  adapter: vercel({ imageService: true }),
+  // adapter: vercel({ imageService: true }),
   output: 'server',
   // Local (standalone)
   // adapter: node({ mode: 'standalone' }),
-  // output: 'server',
+  output: 'static',
 
   // [Assets]
   image: {
